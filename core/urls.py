ba0bd3dc9cf48dc196d7c6b48608a999d6d3583a
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2024-01-21 06:52:50 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from schema_graph.views import Schema
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/', include('apps.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/doc/',
+         SpectacularSwaggerView.as_view(url_name='schema'), name='api_doc'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
