6edb45a660d8836e195c253bd6b7467043c45ff4
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2024-03-28 00:37:53 +0300

    Update serializers.py

--- core/apps/banners/serializers.py
+++ core/apps/banners/serializers.py
@@ -4,6 +4,12 @@ from ..models import Banner
 
 
 class BannerSerializers(serializers.ModelSerializer):
+    time_created = serializers.DateTimeField(
+        format='%Y-%m-%d %H:%M', read_only=True)
+    end_time = serializers.DateTimeField(
+        format='%Y-%m-%d %H:%M', read_only=True)
+    start_time = serializers.DateTimeField(
+        format='%Y-%m-%d %H:%M', read_only=True)
 
     class Meta:
         fields = "__all__"
