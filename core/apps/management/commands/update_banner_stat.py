e3bbe34d09baefce99d5382b44960f52b4d541a6
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2024-03-30 03:31:44 +0300

    Update update_banner_stat.py

--- core/apps/management/commands/update_banner_stat.py
+++ core/apps/management/commands/update_banner_stat.py
@@ -3,12 +3,14 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 from ...models import Banner
 from django.db.models import Q
+import io
+
 
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any):
-        
-        banner = Banner.objects.filter(Q(end_time__lte=timezone.now())).update(is_active=False)
-        
-        print(banner)
-        
-        
+
+        # banner = Banner.objects.filter(
+        #     Q(end_time__lte=timezone.now())).update(is_active=False)
+        file = io.open('test.txt', 'w')
+        file.write("True in time :"+f"{timezone.now()}")
+        # print(banner)
