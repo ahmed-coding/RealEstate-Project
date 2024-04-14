976a2942e175767d87af55c33f40b92ee67bf7eb
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2024-04-15 02:16:01 +0300

    complete alarms views and serializers

---
+++ core/apps/alarms/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from ..models import Alarm, Alarm_value
+
+
+class CreateAlarmSerializer(serializers.ModelSerializer):
+    """Creates a new alarm along with associated alarm values.
+    Example Request Body:
+    ```
+        {
+            "state": 1,
+            "category": 1,
+            "is_active": true,
+            "max_price": 100000,
+            "min_price": 50000,
+            "for_sale": true,
+            "for_rent": false,
+            "alarm_values": [
+                {
+                    "attribute_id": 1,
+                    "value": "3"
+                },
+                {
+                    "attribute_id": 2,
+                    "value": "200.5"
+                },
+                {
+                    "attribute_id": 3,
+                    "value": "true"
+                }
+            ]
+        }
+    ```
+    """
+
+    alarm_values = serializers.ListField(child=serializers.DictField())
+    user = serializers.HiddenField(default=None)
+
+    class Meta:
+        model = Alarm
+        fields = '__all__'
+
+    def validate(self, attrs):
+        attrs['user'] = self.context.get('user', None)
+        # self.address.context = self.context
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        user = self.context.get('user', None)
+
+        alarm_values_data = validated_data.pop('alarm_values')
+        alarm = Alarm.objects.create(**validated_data)
+        for alarm_value_data in alarm_values_data:
+            attribute_id = alarm_value_data.pop('attribute_id')
+            value = alarm_value_data.pop('value')
+            alarm_value = Alarm_value.objects.create(
+                alarm=alarm, attribute_id=attribute_id, value=value)
+        return alarm
--- core/apps/alarms/views.py
+++ core/apps/alarms/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.db.models import Count, Case, When, IntegerField
+from django.utils.translation import gettext_lazy as _
+from rest_framework.decorators import action
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
+from ..pagination import StandardResultsSetPagination
+from rest_framework.views import Response
+from ..models import Property, User
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Count, Avg, Q
+from . import serializers
+from rest_framework import generics
 
-# Create your views here.
+
+class AlarmViewsets(viewsets.ModelViewSet):
+    serializer_class = serializers.CreateAlarmSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = '__all__'
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        """Creates a new alarm along with associated alarm values.
+    Example Request Body:
+    ```
+        {
+            "state": 1,
+            "category": 1,
+            "is_active": true,
+            "max_price": 100000,
+            "min_price": 50000,
+            "for_sale": true,
+            "for_rent": false,
+            "alarm_values": [
+                {
+                    "attribute_id": 1,
+                    "value": "3"
+                },
+                {
+                    "attribute_id": 2,
+                    "value": "200.5"
+                },
+                {
+                    "attribute_id": 3,
+                    "value": "true"
+                }
+            ]
+        }
+    ```
+    """
+        return super().create(request, *args, **kwargs)
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context = context + {'user': self.request.user}
+        return context
