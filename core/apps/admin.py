af1e8246dd11b9ddb252e3f4fa4815acff94f120
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2024-04-20 01:32:17 +0300

    Update admin.py

--- core/apps/admin.py
+++ core/apps/admin.py
@@ -28,7 +28,6 @@ from django.core.files import File
 from firebase_admin import firestore
 
 
-
 class CustomUserChangeForm(forms.ModelForm):
     """
     Custom UserChangForm For AdminUser registertions
@@ -95,25 +94,25 @@ class CustomAdminUser(UserAdmin):
         ),
     )
     form = CustomUserChangeForm
-    
+
     def save_model(self, request, obj, form, change):
         # Save the user object in Django database
         obj.save()
 
-        # Check if the user is newly created (not updated)
-        if not change:
-            # Add the user to Firebase
-            db = firestore.client()
-            users_ref = db.collection('Users')
-            users_ref.document(str(obj.id)).set({
-                'email': obj.email,
-                'fullName': obj.name,
-                'userType': obj.user_type,
-                'phone_number': obj.phone_number,
-                'imageUrl': obj.image.url if obj.image else '',
-                # Add other fields as needed
-            }, merge=True)
-    
+        # # Check if the user is newly created (not updated)
+        # if not change:
+        #     # Add the user to Firebase
+        #     db = firestore.client()
+        #     users_ref = db.collection('Users')
+        #     users_ref.document(str(obj.id)).set({
+        #         'email': obj.email,
+        #         'fullName': obj.name,
+        #         'userType': obj.user_type,
+        #         'phone_number': obj.phone_number,
+        #         'imageUrl': obj.image.url if obj.image else '',
+        #         # Add other fields as needed
+        #     }, merge=True)
+
     # add_form = CustomUserCreationForm
     change_password_form = AdminPasswordChangeForm
     list_display = ("email", "name",
