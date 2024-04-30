84c5b4a7b8c553872149376de88264dd9d827e89
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2024-05-01 00:47:57 +0300

    Update views.py

--- core/apps/friend/views.py
+++ core/apps/friend/views.py
@@ -1,13 +1,7 @@
-from django.shortcuts import render
-import json
-
 # Create your views here.
-
 from rest_framework import viewsets
-from rest_framework import status
 from rest_framework import filters
 from . import serializers
-from ..models import FriendList, User
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from ..models import FriendList, PrivateChatRoom, User
 from django.db.models import Q
