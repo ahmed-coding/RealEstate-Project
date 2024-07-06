ee93c58bf19e9d8c1a40ee830edea192b64a34e5
DheyaAlqaderi <diaa.alqadrei@gmail.com>
2024-07-06 03:39:00 +0300

    upload only one image to feature property

--- core/apps/serializers.py
+++ core/apps/serializers.py
@@ -6,63 +6,64 @@ from django.contrib.contenttypes.models import ContentType
 class Image_Serializers(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ("image", "id")
+        fields = ("image","id")
 
     # def to_representation(self, instance):
     #     return instance.image.url
 
 
 class CreatePropertyfeaturedImage_Serializers(serializers.ModelSerializer):
-    feature_property_image = Image_Serializers(many=True, required=False)
+    # images = Image_Serializers(many=True, required=False)
+
+    image = serializers.ImageField()  # Use ImageField to handle file uploads
 
     class Meta:
         model = Feature_property
-        fields = "__all__"
+        fields = ['property', 'feature', 'image']
 
     def create(self, validated_data):
-        """
-        Create a Feature_property instance and associated images.
-
-        Example JSON payload:
-        {
-            "property": 1,
-            "feature": 2,
-            "feature_property_image": [
-                {
-                    "image": "image1.jpg"
-                },
-                {
-                    "image": "image2.jpg"
-                },
-                {
-                    "image": "image3.jpg"
-                }
-            ]
-        }
-
-        Parameters:
-        validated_data (dict): The validated data from the serializer.
-
-        Returns:
-        Feature_property: The created Feature_property instance with associated images.
-
-        Explanation of JSON Fields:
-        - property: The ID of the Property instance to which this Feature_property is related.
-        - feature: The ID of the Feature instance to which this Feature_property is related.
-        - feature_property_image: A list of image objects, where each object contains the image field representing the image file name or path.
-
-        Note:
-        - The IDs (property and feature) should correspond to existing instances in your database.
-        - The image fields in feature_property_image should contain valid paths or names of the image files that will be processed by the ImageField.
-        """
-        images_data = validated_data.pop('feature_property_image', [])
-        feature_property_instance = Feature_property.objects.create(
-            **validated_data)
-        content_type = ContentType.objects.get_for_model(Feature_property)
-        for image_data in images_data:
-            Image.objects.create(object_id=feature_property_instance.id,
-                                 content_type=content_type, image=image_data['image'])
+        image_file = validated_data.pop('image')
+        feature_property_instance = Feature_property.objects.create(**validated_data)
+        
+        Image.objects.create(content_object=feature_property_instance, image=image_file)
+        
         return feature_property_instance
+    # class Meta:
+    #     model = Feature_property
+    #     fields = "__all__"
+
+    # def create(self, validated_data):
+    #     """
+    #     Create a Feature_property instance and associated images.
+
+    #     Example JSON payload:
+    #     {
+    #         "property": 1,
+    #         "feature": 2,
+    #         "image": "image3.jpg"
+    #     }
+
+    #     Parameters:
+    #     validated_data (dict): The validated data from the serializer.
+
+    #     Returns:
+    #     Feature_property: The created Feature_property instance with associated images.
+
+    #     Explanation of JSON Fields:
+    #     - property: The ID of the Property instance to which this Feature_property is related.
+    #     - feature: The ID of the Feature instance to which this Feature_property is related.
+    #     - feature_property_image: A list of image objects, where each object contains the image field representing the image file name or path.
+
+    #     Note:
+    #     - The IDs (property and feature) should correspond to existing instances in your database.
+    #     - The image fields in feature_property_image should contain valid paths or names of the image files that will be processed by the ImageField.
+    #     """
+    #     image_data = validated_data.pop('image')
+    #     feature_property_instance = Feature_property.objects.create(**validated_data)
+        
+    #     Image.objects.create(feature_property=feature_property_instance, image=image_data)
+        
+    #     return feature_property_instance
 
     # def to_representation(self, instance):
     #     return instance.image.url
