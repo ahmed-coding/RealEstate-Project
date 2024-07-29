72d6a507a2885f0c248af15f030dee6005b2f84e
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2024-07-30 00:11:34 +0300

    fix urls router and views

--- core/apps/authentication/urls.py
+++ core/apps/authentication/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', views.CustomAuthToken.as_view()),
     path('sginup/', views.ReigsterView.as_view()),
     path('logout/', views.LogoutView.as_view()),
-    path('verify-email/', views.verify_email, name='verify_email'),
+    path('verify-email/', views.check_email_velidate, name='verify_email'),
     path('send-verify-email/', views. send_verify_email, name='send_verify_email'),
     path('reset-password/', views.PasswordResetView.as_view(),
          name='reset-password /'),
